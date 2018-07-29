40b6e6f27526d521992a55a2cf021fd70fd8b2b4
Akshaya <akshaya.swaroop3291@gmail.com>
2018-07-29 21:40:32 +0530

    renters and houseowners endpoint added

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask, request, jsonify, make_response, render_template
-from flask_restplus import Api, Resource
+from flask_restplus import Api, Resource, fields
 from flask_mail import Mail, Message
 from datetime import datetime
 from email_validator import validate_email, EmailNotValidError
@@ -14,6 +14,8 @@ uri = "mongodb://127.0.0.1:27017"
 client = pymongo.MongoClient(uri)
 database = client['moovintodb']
 users = database['users']
+renters = database['renters']
+houseowners = database['houseowners']
 
 app = Flask(__name__, template_folder='templates')
 # Setup the Flask-JWT-Extended extension
@@ -146,11 +148,14 @@ class Register(Resource):
                 return make_response(jsonify({"success": "false", "status_code": 401, "payload": {},
                         "error": {"message": "Unauthorized"}}), 401)
 
+get_user_parser = api.parser()
+get_user_parser.add_argument('API-TOKEN', location='headers')
+
 @api.route('/user/<int:user_id>')
-@api.doc(params={'id': 'An ID', 'API-TOKEN': 'API Token in headers'})
 class GetUser(Resource):
     @api.response(200, 'Success')
     @api.response(403, 'Not Authorized')
+    @api.expect(get_user_parser)
     def get(self, user_id):
         # check Api token exists
         api_token = request.headers['API-TOKEN']
@@ -185,15 +190,19 @@ class GetUser(Resource):
                                           "error": {"message": "Unauthorized"}}), 403)
 
 
+update_user_parser = api.parser()
+update_user_parser.add_argument('API-TOKEN', location='headers')
+
 @api.route('/update-user')
-@api.doc(params={'api-token': 'API Token in post data'})
+@api.doc(params={'firstname': 'Firstname', 'lastname': 'Lastname', 'email': 'Email', 'usertype': 'User Type', 'userstatus': 'User Status', 'password': 'Password'})
 class UpdateUser(Resource):
     @api.response(200, 'Success')
     @api.response(403, 'Not Authorized')
+    @api.expect(update_user_parser)
     def post(self):
         if request.get_json():
             data = request.get_json()
-            api_token = data['api-token']
+            api_token = request.headers['API-TOKEN']
             firstname = data['firstname']
             lastname = data['lastname']
             email = data['email']
@@ -300,6 +309,88 @@ class ResetPassword(Resource):
                 return make_response(jsonify({"success": "false", "status_code": 403, "payload": {},
                                           "error": {"message": "Unauthorized"}}), 403)
 
+
+renters_resource = api.parser()
+renters_resource.add_argument('API-TOKEN', location='headers')
+@api.route('/user/update-renters-data')
+@api.doc(params={'accommodation_for': 'Looking for a place for', 'accommodation_wanted_applicants': 'data{}', 'teamup': 'Share house together', 'where_to_live': 'Location', 'max_budget': 'Max budget', 'move_date': 'Move Date', 'preferred_length_of_stay': 'Length of stay', 'about_renter': 'About Renter', 'renter_description': 'Renter Description', 'roommate_preferences': 'Roommate Preferences', 'email': 'Email'})
+class UpdateRentersData(Resource):
+    @api.response(200, 'Success')
+    @api.response(403, 'Not Authorized')
+    @api.expect(renters_resource)
+    def post(self):
+        if request.get_json():
+            api_token = request.headers['API-TOKEN']
+            data = request.get_json()
+
+            if not api_token:
+                return make_response(jsonify({"success": "false", "status_code": 403, "payload": {},
+                                              "error": {"message": "Unauthorized"}}), 403)
+
+            # check api token exists in db
+            register_user = users.find_one({"api_token": api_token})
+
+            if register_user:
+                newrenter = {
+                    "accommodation_for": data['accommodation_for'],
+                    "accommodation_wanted_applicants": data['accommodation_wanted_applicants'],
+                    "teamup": data['teamup'],
+                    "where_to_live": data['where_to_live'],
+                    "max_budget": data['max_budget'],
+                    "move_date": data['move_date'],
+                    "preferred_length_of_stay": data['preferred_length_of_stay'],
+                    "about_renter": data['about_renter'],
+                    "renter_description": data['renter_description'],
+                    "roommate_preferences": data['roommate_preferences'],
+                    "email": register_user['email']
+                }
+                database.renters.insert_one(newrenter)
+                return make_response(jsonify({"success": "true", "status_code": 200, "payload": newrenter}), 200)
+            else:
+                return make_response(jsonify({"success": "false", "status_code": 403, "payload": {},
+                                              "error": {"message": "Unauthorized"}}), 403)
+
+        else:
+            return make_response(jsonify({"success": "false", "status_code": 403, "payload": {},
+                                      "error": {"message": "Unauthorized"}}), 403)
+
+
+
+houseowners_resource = api.parser()
+houseowners_resource.add_argument('API-TOKEN', location='headers')
+@api.route('/user/update-houseowners-data')
+@api.doc(params={'accommodation_for': 'Looking for a place for', 'accommodation_wanted_applicants': 'data{}', 'teamup': 'Share house together', 'where_to_live': 'Location', 'max_budget': 'Max budget', 'move_date': 'Move Date', 'preferred_length_of_stay': 'Length of stay', 'about_renter': 'About Renter', 'renter_description': 'Renter Description', 'roommate_preferences': 'Roommate Preferences', 'email': 'Email'})
+class UpdateHouseownersData(Resource):
+    @api.response(200, 'Success')
+    @api.response(403, 'Not Authorized')
+    @api.expect(houseowners_resource)
+    def post(self):
+        if request.get_json():
+            api_token = request.headers['API-TOKEN']
+            data = request.get_json()
+
+            if not api_token:
+                return make_response(jsonify({"success": "false", "status_code": 403, "payload": {},
+                                              "error": {"message": "Unauthorized"}}), 403)
+
+            # check api token exists in db
+            register_user = users.find_one({"api_token": api_token})
+
+            if register_user:
+                newhouseowner = {
+                    "renter_preferences": data['renter_preferences'],
+                    "property_id": data['property_id'],
+                    "email": register_user['email']
+                }
+                database.renters.insert_one(newhouseowner)
+                return make_response(jsonify({"success": "true", "status_code": 200, "payload": newhouseowner}), 200)
+            else:
+                return make_response(jsonify({"success": "false", "status_code": 403, "payload": {},
+                                              "error": {"message": "Unauthorized"}}), 403)
+
+        else:
+            return make_response(jsonify({"success": "false", "status_code": 403, "payload": {},
+                                      "error": {"message": "Unauthorized"}}), 403)
 
 if __name__ == '__main__':
     app.run(debug=True)
